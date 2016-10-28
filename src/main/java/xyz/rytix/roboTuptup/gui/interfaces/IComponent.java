169f23bc067d4a356f33fac2184479e716a198c8
rytix <aff12300@gmail.com>
2016-10-28 08:34:31 -0200

    Adicionando Interface e outros arquivos

---
+++ src/main/java/xyz/rytix/roboTuptup/gui/interfaces/IComponent.java
@@ -0,0 +1,20 @@
+package xyz.rytix.roboTuptup.gui.interfaces;
+
+import net.minecraft.client.renderer.Tessellator;
+
+public interface IComponent {
+	
+	public IComponent getComponentOn(int mouseX,int mouseY);
+	public boolean isMouseInside(int mouseX,int mouseY);
+	public void draw(Tessellator tessellator);
+	
+	public int getTrueLeft();
+	public int getTrueTop();
+	
+	public int getLeft();
+	public void setLeft(int left);
+	public int getTop();
+	public void setTop(int top);
+	public int getWidth();
+	public int getHeight();
+}
